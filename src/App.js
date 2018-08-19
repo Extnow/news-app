673e6aaeecfe26268407fca3b90f4a2bd2fef2f9
Ryabushkin Mikhail <ryabushkin.mikhail@gmail.com>
2018-08-19 13:35:42 +0300

    fix eslint errors with App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
-}
+export default () => (
+  <div className="App" />
+);
